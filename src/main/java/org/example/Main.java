9eccbbd5a9fd53bced9e930cc7024861e05c14c9
hwshin1 <hwshin5044@gmail.com>
2025-04-13 11:31:52 +0900

    Window 창 생성

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Java Swing practice");
+        MainWindow mw = new MainWindow();
+        mw.show();
+    }
+}
+
+class MainWindow {
+    private JFrame window;
+
+    public MainWindow() {
+        window = new JFrame();
+        window.setTitle("Test Window");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setSize(1280, 720);
+        window.setLocationRelativeTo(null);
+    }
+
+    public void show() {
+        window.setVisible(true);
     }
 }
