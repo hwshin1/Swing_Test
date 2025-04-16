8b4f67b621cd3da8e896d1a1f618773a2cc2df73
hwshin1 <hwshin5044@gmail.com>
2025-04-16 22:27:37 +0900

    JFrame class 및 버튼 생성

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,8 +4,14 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        MainWindow mw = new MainWindow();
-        mw.show();
+        // MainWindow mw = new MainWindow();
+        // mw.show();
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                JFrameOne frameOne = new JFrameOne();
+                frameOne.setVisible(true);
+            }
+        });
     }
 }
 
@@ -22,5 +28,22 @@ class MainWindow {
 
     public void show() {
         window.setVisible(true);
+    }
+}
+
+class JFrameOne extends JFrame {
+    public JFrameOne() {
+        initialize();
+    }
+
+    public void initialize() {
+        setTitle("JFrameOne");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(540, 300);
+
+        setLayout(null);
+        JButton button = new JButton("Button");
+        button.setBounds(10, 10, 100, 30);
+        add(button);
     }
 }
